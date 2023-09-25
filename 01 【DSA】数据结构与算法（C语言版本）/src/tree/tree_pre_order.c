e451dc12381748e8513dd53628c26f45896f115b
lambertt <lambertt1702@163.com>
2023-09-26 02:45:19 +0800

    【修改说明】修改二叉树遍历代码和图片

---
+++ 01 【DSA】数据结构与算法（C语言版本）/src/tree/tree_pre_order.c
@@ -0,0 +1,81 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define ARRAY_MAX_SIZE 100
+typedef struct BiTNode {
+    int data;
+    struct BiTNode *lchild, *rchild;
+} BiTNode, *BiTree;
+
+void PreOrderTraverse(BiTree binaryTree)
+{
+    if (binaryTree == NULL) {
+        return;
+    }
+
+    printf("%d\n", binaryTree->data);
+
+    PreOrderTraverse(binaryTree->lchild);
+    PreOrderTraverse(binaryTree->rchild);
+}
+
+/**********************
+    测试代码框架
+    二叉树结构：
+        1
+       /  \
+      2    3 
+     / \   / 
+    4   5 6
+   /
+  7
+**********************/ 
+
+// 程序执行即结束，free
+BiTree CreateBiTree()
+{
+    // 1
+    BiTree tree = NULL;
+    tree = (BiTNode*)malloc(sizeof(BiTNode));
+    tree->data = 1;
+
+    // 2、3
+    BiTree tree_2 = (BiTNode*)malloc(sizeof(BiTNode));
+    tree_2->data = 2;
+    BiTree tree_3 = (BiTNode*)malloc(sizeof(BiTNode));
+    tree_3->data = 3;
+    tree->lchild = tree_2, tree->rchild = tree_3;
+    tree_3->rchild = NULL;
+
+    // 4、5
+    BiTree tree_4 = (BiTNode*)malloc(sizeof(BiTNode));
+    tree_4->data = 4;
+    BiTree tree_5 = (BiTNode*)malloc(sizeof(BiTNode));
+    tree_5->data = 5;
+    tree_2->lchild = tree_4, tree_2->rchild = tree_5;
+
+    tree_4->rchild = NULL;
+    tree_5->lchild = NULL, tree_5->rchild = NULL;
+
+    // 6
+    BiTree tree_6 = (BiTNode*)malloc(sizeof(BiTNode));
+    tree_6->data = 6;
+    tree_3->lchild = tree_6;
+
+    tree_6->lchild = NULL, tree_6->rchild = NULL;
+
+    // 7
+    BiTree tree_7 = (BiTNode*)malloc(sizeof(BiTNode));
+    tree_7->data = 7;
+    tree_4->lchild = tree_7;
+    tree_7->lchild = NULL, tree_7->rchild = NULL;
+    
+    return tree;
+}
+
+int main()
+{
+    BiTree tree = CreateBiTree();
+    PreOrderTraverse(tree);
+}
